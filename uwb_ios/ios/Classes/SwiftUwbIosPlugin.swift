3efacfca8922ffa94b7cb1a58154cd02d6aef85a
Leroy <just-leroy@hotmail.com>
2022-03-01 11:35:21 +0100

    removed calculator example class

--- uwb_ios/ios/Classes/SwiftUwbIosPlugin.swift
+++ uwb_ios/ios/Classes/SwiftUwbIosPlugin.swift
@@ -4,7 +4,6 @@ import UIKit
 @available(iOS 14.0, *)
 public class SwiftUwbIosPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-      MCalculatorHostApi.setUp(binaryMessenger: registrar.messenger())
       NISessionHostApi.setUp(binaryMessenger: registrar.messenger())
       MCSessionHostApi.setUp(binaryMessenger: registrar.messenger())
   }
