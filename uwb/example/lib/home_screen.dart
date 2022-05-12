c9113c058b9606c2b17b07c4febadfc54abb3034
Leroy <just-leroy@hotmail.com>
2022-05-12 11:32:22 +0200

    added homescreen file and breakout game file

---
+++ uwb/example/lib/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'breakout_game.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BreakoutGame()));
+              },
+              child: const Text(
+                'P L A Y',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BreakoutGame()));
+              },
+              child: const Text(
+                'H O S T',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
