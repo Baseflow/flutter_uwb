957e391f9ee8c59369fd3d7576b7d18d621b4e27
Leroy <just-leroy@hotmail.com>
2022-06-14 15:35:32 +0200

    added requested changes. Adjusted return types

--- uwb_platform_interface/lib/uwb_platform_interface.dart
+++ uwb_platform_interface/lib/uwb_platform_interface.dart
@@ -23,30 +23,29 @@ abstract class UwbPlatform extends PlatformInterface {
   /// Should only be accessed after setter is called.
   static late UwbPlatform _instance;
 
-  /// Setup call for initial setup.
+  /// Checks platform compatibility.
   ///
-  /// Must be called once.
-  /// Sets up the reverse method channel for location updates.
-  /// Checks device compatibility with nearby interaction. Returns true of compatible.
-  Future<bool?> setUp() =>
+  /// Returns [True] of compatible.
+  /// Returns [False] if it is not compatible.
+  Future<bool> checkPlatformCompatibility() =>
       throw UnimplementedError('setUp() has not been implemented.');
 
   /// Start advertising as a ultra wide band host allowing clients to connect.
   ///
   /// The [peerID] identifies a peer (the host in this case) in a multi peer session.
   /// The [serviceType] describes the service to advertise. This should be a short text string that describes the app's networking protocol.
-  Future<bool?> startHost(String peerID, String serviceType) =>
+  Future<void> startHost(String peerID, String serviceType) =>
       throw UnimplementedError('startHost() has not been implemented.');
 
   /// Sends an invite to the advertiser to start a multi peer session.
   ///
   /// The [peerID] identifies a peer (the client in this case) in a multi peer session.
   /// The [serviceType] describes the service to advertise. This should be a short text string that describes the app's networking protocol.
-  Future<bool?> joinHost(String peerID, String serviceType) =>
+  Future<void> joinHost(String peerID, String serviceType) =>
       throw UnimplementedError('joinHost() has not been implemented.');
 
   /// Callback method to get the location updates.
-  Future<bool?> getLocation(
+  Future<void> getLocation(
           {required Function(Map<dynamic, dynamic>) onLocation}) =>
       throw UnimplementedError('getLocation() has not been implemented.');
 }
