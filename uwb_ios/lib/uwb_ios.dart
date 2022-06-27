e99d47ec7f8550f7e2165f73023c295bfb7d7ba0
Leroy <just-leroy@hotmail.com>
2022-06-27 17:58:54 +0200

    fixed build issue by adding register method in UbwIos

--- uwb_ios/lib/uwb_ios.dart
+++ uwb_ios/lib/uwb_ios.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
+import 'package:uwb_platform_interface/uwb_platform_interface.dart';
 
 /// The [UwbIos] class.
 ///
 /// Contains all the methodchannel calls.
-class UwbIos {
+class UwbIos extends UwbPlatform {
   static const MethodChannel _channel = MethodChannel('uwb_ios');
+
+  /// Sets the UbwPlatform instance
+  static void registerWith() {
+    UwbPlatform.instance = UwbIos();
+  }
 
   /// Methoc call that gets the platformversion from iOS.
   Future<String?> get platformVersion async {
