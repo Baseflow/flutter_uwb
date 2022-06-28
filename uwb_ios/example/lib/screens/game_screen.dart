d2d2f3d77acd4e413d487f6fb0a0dcf630c2a4ae
Leroy <just-leroy@hotmail.com>
2022-06-28 19:30:55 +0200

    added join example in game_screen.dart in example/lib/screens directory

--- uwb_ios/example/lib/screens/game_screen.dart
+++ uwb_ios/example/lib/screens/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uwb_ios/uwb_ios.dart';
 
 /// The [BreakoutGame] class.
 ///
@@ -8,10 +9,30 @@ class BreakoutGame extends StatefulWidget {
   const BreakoutGame({Key? key}) : super(key: key);
 
   @override
-  State<BreakoutGame> createState() => _BreakoutGameState();
+  State<BreakoutGame> createState() => BreakoutGameState();
 }
 
-class _BreakoutGameState extends State<BreakoutGame> {
+/// The [BreakoutGameState] class.
+class BreakoutGameState extends State<BreakoutGame> {
+  final UwbIos _plugin = UwbIos();
+
+  /// The [deviceName] that is shown to peers when advertising.
+  String deviceName = 'test-device';
+
+  /// The [serviceType] describes the service to advertise. This should be a short text string that describes the app's networking protocol.
+  String serviceType = 'uwb-test';
+
+  @override
+  void initState() {
+    joinHost();
+    super.initState();
+  }
+
+  /// Join host call from the uwb plugin to join a host phone and setup a connection.
+  Future<void> joinHost() async {
+    await _plugin.joinHost(deviceName, serviceType);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
